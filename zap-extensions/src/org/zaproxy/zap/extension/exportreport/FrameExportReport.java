4ebd24d4c2ee8e11238ccb1c9b44d11e6651244d
JordanGS <developer.goran@gmail.com>
2016-08-17 15:44:51 -0400

    Export Report Commit Change Log for version 1.0
    29) Renamed all packages to lowercase
    30) Removed unused import JOptionPane from ExportReport
    31) Added static final to Logger's
    32) Added private static to Task class in ExportReport and modified subsequent methods to static
    33) Changed XML_PATH to xmlPath
    34) Added return null to UnsupportedEncodingException in ExportReport
    35) Remove redundant parenthesis from ExportReport
    36) Changed method name TRANSFORMATION to transformation
    37) Removed al J Element parameters from methods
    38) Changed all to final static since that is expected order.
    39) path + fileName + ".xml" is now a variable
    40) Removed unnecessary null assignments
    41) Updated negation rules for file displayed
    42) Removed trailing white space characters
    43) Renamed bool to valid for ease of understanding
    44) Removed WritableFileChooser parameter
    45) Changed to StandardCharsets.ISO_8859_1
    46) Changed to StandardCharsets.UTF_8.name() and removed global variable from Utils
    47) Removed fc.getSelectedFile from doInBackground method and added a File f variable to the Task constructor
    48) Create native loggers for each class and removed the use of custom loggers
    49) Removed 'f_view = null' resets, except for the first initialization
    50) deleteFile , parameter err removed and it's usage changed
    51) Added finally block to deleteFile to ensure that's deleted in case of exception
    52) Changed boolean bool to valid
    53) Map the risk values to the translations
    54) Call alert.getMessage() only once and only if it's needed.
    55) transformation and jsonExport removed the absolutePath parameter, determine the use
    56) Removed the custom logger in the above
    57) Removed the ExtensionExportReport constructor with parameter
    59) Changed all Utils constants to upper case
    59) Added command line options to the plugin
    60) Removed initilizeList and replaced it with init

--- zap-extensions/src/org/zaproxy/zap/extension/exportreport/FrameExportReport.java
+++ zap-extensions/src/org/zaproxy/zap/extension/exportreport/FrameExportReport.java
@@ -1,19 +1,19 @@
 /*
  * Zed Attack Proxy (ZAP) and its related class files.
- * 
+ *
  * ZAP is an HTTP/HTTPS proxy for assessing web application security.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * This file is based on the Paros code file ReportLastScan.java
  */
 package org.zaproxy.zap.extension.exportreport;
@@ -27,128 +27,111 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
 import javax.swing.border.CompoundBorder;
-import org.parosproxy.paros.view.AbstractFrame;
+
 import org.parosproxy.paros.Constant;
+import org.parosproxy.paros.view.AbstractFrame;
 
 /*
- * AUTHOR		:	GORAN SARENKAPA - JordanGS
- * SPONSOR		:	RYERSON UNIVERSITY
+ * AUTHOR: GORAN SARENKAPA - JordanGS
+ * SPONSOR: RYERSON UNIVERSITY
  */
 
-public class FrameExportReport extends AbstractFrame implements ActionListener
-{
-	private final String			FINISH				= Constant.messages.getString("exportreport.forward.finish.label");
-	private final String			NEXT				= Constant.messages.getString("exportreport.forward.next.label");
-	private ExtensionExportReport	extension			= null;
-	private JPanel					pnlCards;
-	private JButton					btnBack;
-	private JButton					btnNext;
-	private PanelNav				pnlNav;
-	private int						curCard				= 0;
-	final String					PANEL_SOURCE		= Constant.messages.getString("exportreport.panel.source.label");
-	final String					PANEL_RISK			= Constant.messages.getString("exportreport.panel.risk.label");
-	final String					PANEL_DETAILS		= Constant.messages.getString("exportreport.panel.details.label");
-	final String					cardItems[]			=
-	{
-			PANEL_SOURCE, PANEL_RISK, PANEL_DETAILS
-	};
+@SuppressWarnings("serial")
+public class FrameExportReport extends AbstractFrame implements ActionListener {
+    private final String FINISH = Constant.messages.getString("exportreport.forward.finish.label");
+    private final String NEXT = Constant.messages.getString("exportreport.forward.next.label");
+    private ExtensionExportReport extension = null;
+    private JPanel pnlCards;
+    private JButton btnBack;
+    private JButton btnNext;
+    private PanelNav pnlNav;
+    private int curCard = 0;
+    final String PANEL_SOURCE = Constant.messages.getString("exportreport.panel.source.label");
+    final String PANEL_RISK = Constant.messages.getString("exportreport.panel.risk.label");
+    final String PANEL_DETAILS = Constant.messages.getString("exportreport.panel.details.label");
+    final String cardItems[] = { PANEL_SOURCE, PANEL_RISK, PANEL_DETAILS };
 
-	public FrameExportReport(ExtensionExportReport extension, PanelSource cardSource, PanelAlertRisk cardAlertRisk, PanelAlertDetails cardAlertDetails)
-	{
-		this.extension = extension;
-		this.setTitle(Constant.messages.getString("exportreport.menu.title.label"));
-		this.addComponentToPane(this.getContentPane(), cardSource, cardAlertRisk, cardAlertDetails);
-	}
+    public FrameExportReport(ExtensionExportReport extension, PanelSource cardSource, PanelAlertRisk cardAlertRisk, PanelAlertDetails cardAlertDetails) {
+        this.extension = extension;
+        this.setTitle(Constant.messages.getString("exportreport.menu.title.label"));
+        this.addComponentToPane(this.getContentPane(), cardSource, cardAlertRisk, cardAlertDetails);
+    }
 
-	public void addComponentToPane(Container contentPane, PanelSource cardSource, PanelAlertRisk cardAlertRisk, PanelAlertDetails cardAlertDetails)
-	{
+    public void addComponentToPane(Container contentPane, PanelSource cardSource, PanelAlertRisk cardAlertRisk, PanelAlertDetails cardAlertDetails) {
 
-		SpringLayout sl_contentPane = new SpringLayout();
-		contentPane.setLayout(sl_contentPane);
+        SpringLayout sl_contentPane = new SpringLayout();
+        contentPane.setLayout(sl_contentPane);
 
-		pnlCards = new JPanel(new CardLayout());
-		pnlCards.add(cardSource, PANEL_SOURCE);
-		pnlCards.add(cardAlertRisk, PANEL_RISK);
-		pnlCards.add(cardAlertDetails, PANEL_DETAILS);
+        pnlCards = new JPanel(new CardLayout());
+        pnlCards.add(cardSource, PANEL_SOURCE);
+        pnlCards.add(cardAlertRisk, PANEL_RISK);
+        pnlCards.add(cardAlertDetails, PANEL_DETAILS);
 
-		pnlCards.setBorder(new CompoundBorder()); // -Give the cards panel a border
-		sl_contentPane.putConstraint(SpringLayout.NORTH, pnlCards, 10, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, pnlCards, 10, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, pnlCards, 345, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, pnlCards, 370, SpringLayout.WEST, contentPane);
-		contentPane.add(pnlCards);
+        pnlCards.setBorder(new CompoundBorder()); // -Give the cards panel a border
+        sl_contentPane.putConstraint(SpringLayout.NORTH, pnlCards, 10, SpringLayout.NORTH, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.WEST, pnlCards, 10, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, pnlCards, 345, SpringLayout.NORTH, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.EAST, pnlCards, 370, SpringLayout.WEST, contentPane);
+        contentPane.add(pnlCards);
 
-		pnlNav = new PanelNav(cardItems.length); // -Use the constructor to initialize the
-		// navigation panel.
-		sl_contentPane.putConstraint(SpringLayout.NORTH, pnlNav, 20, SpringLayout.SOUTH, pnlCards);
-		sl_contentPane.putConstraint(SpringLayout.WEST, pnlNav, 10, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, pnlNav, 100, SpringLayout.SOUTH, pnlCards);
-		sl_contentPane.putConstraint(SpringLayout.EAST, pnlNav, 0, SpringLayout.EAST, pnlCards);
+        pnlNav = new PanelNav(cardItems.length); // -Use the constructor to initialize the navigation panel.
+        sl_contentPane.putConstraint(SpringLayout.NORTH, pnlNav, 20, SpringLayout.SOUTH, pnlCards);
+        sl_contentPane.putConstraint(SpringLayout.WEST, pnlNav, 10, SpringLayout.WEST, contentPane);
+        sl_contentPane.putConstraint(SpringLayout.SOUTH, pnlNav, 100, SpringLayout.SOUTH, pnlCards);
+        sl_contentPane.putConstraint(SpringLayout.EAST, pnlNav, 0, SpringLayout.EAST, pnlCards);
 
-		// Assign buttons from the navigation pane action listeners.
-		this.btnBack = pnlNav.getBack();
-		this.btnBack.addActionListener(this);
-		this.btnNext = pnlNav.getNext();
-		this.btnNext.addActionListener(this);
+        // Assign buttons from the navigation pane action listeners.
+        this.btnBack = pnlNav.getBack();
+        this.btnBack.addActionListener(this);
+        this.btnNext = pnlNav.getNext();
+        this.btnNext.addActionListener(this);
 
-		contentPane.add(pnlNav); // -Add the navigation panel to the content
-	}
+        contentPane.add(pnlNav); // -Add the navigation panel to the content
+    }
 
-	private int incCard(int curCard)
-	{
-		if (curCard < cardItems.length - 1)
-			curCard = curCard + 1;
-		return curCard;
-	}
+    private int incCard(int curCard) {
+        if (curCard < cardItems.length - 1)
+            curCard = curCard + 1;
+        return curCard;
+    }
 
-	private int decCard(int curCard)
-	{
-		if (curCard > 0)
-			curCard = curCard - 1;
-		return curCard;
-	}
+    private int decCard(int curCard) {
+        if (curCard > 0)
+            curCard = curCard - 1;
+        return curCard;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		if (e.getSource() == btnBack)
-		{
-			curCard = decCard(curCard);
-			CardLayout cl = (CardLayout) (pnlCards.getLayout());
-			cl.show(pnlCards, cardItems[curCard]);
-			if (curCard > 0)
-			{
-				pnlNav.setProgress(curCard + 1);
-				btnNext.setText(NEXT);
-			}
-			else
-			{
-				pnlNav.setProgress(1);
-				btnBack.setEnabled(false);
-			}
-		}
-		if (e.getSource() == this.btnNext)
-		{
-			if (btnNext.getText() == FINISH)
-			{
-				extension.generateReport();
-			}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnBack) {
+            curCard = decCard(curCard);
+            CardLayout cl = (CardLayout) (pnlCards.getLayout());
+            cl.show(pnlCards, cardItems[curCard]);
+            if (curCard > 0) {
+                pnlNav.setProgress(curCard + 1);
+                btnNext.setText(NEXT);
+            } else {
+                pnlNav.setProgress(1);
+                btnBack.setEnabled(false);
+            }
+        }
+        if (e.getSource() == this.btnNext) {
+            if (btnNext.getText() == FINISH) {
+                extension.generateReport();
+            }
 
-			curCard = incCard(curCard);
+            curCard = incCard(curCard);
 
-			CardLayout cl = (CardLayout) (pnlCards.getLayout());
-			cl.show(pnlCards, cardItems[curCard]);
-			if (curCard < (cardItems.length - 1))
-			{
-				pnlNav.setProgress(curCard + 1);
-				btnBack.setEnabled(true);
-			}
-			else
-			{
-				pnlNav.setProgress(cardItems.length);
-				btnNext.setText(FINISH);
-			}
+            CardLayout cl = (CardLayout) (pnlCards.getLayout());
+            cl.show(pnlCards, cardItems[curCard]);
+            if (curCard < (cardItems.length - 1)) {
+                pnlNav.setProgress(curCard + 1);
+                btnBack.setEnabled(true);
+            } else {
+                pnlNav.setProgress(cardItems.length);
+                btnNext.setText(FINISH);
+            }
 
-		}
-	}
+        }
+    }
 }
